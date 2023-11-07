daac5fa8f1b16e9453fa4de2103952d4b1fd01de
Ansar857 <mohammadansar857@gmail.com>
2023-11-08 03:41:14 +0500

    new commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,28 +1,32 @@
-'use client'
-import { Box,Image, Input, Text, chakra } from "@chakra-ui/react";
+"use client";
+import { Box, Image, Input, Text, chakra } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
-import React, { useState } from 'react';
-import {CognitoIdentityProviderClient,InitiateAuthCommand,RespondToAuthChallengeCommand,} from '@aws-sdk/client-cognito-identity-provider';
+import React, { useState } from "react";
+import {
+  CognitoIdentityProviderClient,
+  InitiateAuthCommand,
+  RespondToAuthChallengeCommand,
+} from "@aws-sdk/client-cognito-identity-provider";
 
-const clientId = '1727702mdj4021tmc218s3efab'; // Replace with your App Client ID
-const cognitoClient = new CognitoIdentityProviderClient({ region: 'us-east-1' }); // Replace with your AWS region
-
+const clientId = "1727702mdj4021tmc218s3efab"; // Replace with your App Client ID
+const cognitoClient = new CognitoIdentityProviderClient({
+  region: "us-east-1",
+}); // Replace with your AWS region
 
 const page = () => {
-
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [session, setSession] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [session, setSession] = useState("");
   const [challengeName, setChallengeName] = useState<string | null>(null);
   const [challengeParams, setChallengeParams] = useState<any>({});
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const router = useRouter();
 
   const handleSignIn = async () => {
     try {
       const authCommand = new InitiateAuthCommand({
-        AuthFlow: 'USER_PASSWORD_AUTH',
+        AuthFlow: "USER_PASSWORD_AUTH",
         ClientId: clientId,
         AuthParameters: {
           USERNAME: username,
@@ -30,63 +34,59 @@ const page = () => {
         },
       });
 
-      const authResponse :any = await cognitoClient.send(authCommand);
+      const authResponse: any = await cognitoClient.send(authCommand);
 
       if (authResponse.ChallengeName) {
         setChallengeName(authResponse.ChallengeName);
         setChallengeParams(authResponse.ChallengeParameters);
-        setSession(authResponse.Session)
+        setSession(authResponse.Session);
       } else {
         // setSession(authResponse.Session);
 
-        setMessage('Login successful.');
-        console.log("Login Successful")
-        router.push('/')
-        
+        setMessage("Login successful.");
+        console.log("Login Successful");
+        router.push("/");
       }
     } catch (error) {
       setMessage(`Error: ${error}`);
-      console.log(error)
+      console.log(error);
     }
   };
 
   const handleChallengeResponse = async () => {
     try {
       const respondToChallengeCommand = new RespondToAuthChallengeCommand({
-        ChallengeName: 'NEW_PASSWORD_REQUIRED',
+        ChallengeName: "NEW_PASSWORD_REQUIRED",
         ClientId: clientId,
-        Session : session,
+        Session: session,
         ChallengeResponses: {
           NEW_PASSWORD: newPassword,
           USERNAME: username,
-        
         },
       });
 
-      const challengeResponse = await cognitoClient.send(respondToChallengeCommand);
+      const challengeResponse = await cognitoClient.send(
+        respondToChallengeCommand
+      );
 
       if (challengeResponse.AuthenticationResult) {
-        setMessage('Password changed successfully.');
+        setMessage("Password changed successfully.");
       } else {
-        setMessage('Challenge response failed.');
+        setMessage("Challenge response failed.");
       }
     } catch (error) {
       setMessage(`Error: ${error}`);
     }
   };
 
-
-  
   return (
     <Box
-    display={'flex'}
-    flexDirection={'column'}
-    margin={'auto'}
-    alignItems={'center'}
-    justifyContent={'center'}
-    height={'100vh'}
-                  
-    
+      display={"flex"}
+      flexDirection={"column"}
+      margin={"auto"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      height={"100vh"}
     >
       <Box
         display={"flex"}
@@ -98,10 +98,6 @@ const page = () => {
         alignItems={"center"}
         gap={{ "2xl": "32px" }}
         background={"var(--base-white, #FFF)"}
-        
-       
-        
-        
       >
         {/* ############# CONTNENT BOX  ########################## */}
 
@@ -130,27 +126,27 @@ const page = () => {
               fill={"var(--primary-main, #11190C)"}
             />
 
-          <Box
-            display={"flex"}
-            flexDirection={"column"}
-            alignItems={"flex-start"}
-            gap={"12px"}
-            alignSelf={"stretch"}
-          >
-            <Text
+            <Box
+              display={"flex"}
+              flexDirection={"column"}
+              alignItems={"flex-start"}
+              gap={"12px"}
               alignSelf={"stretch"}
-              color={"var(--gray-900, #101828)"}
-              textAlign={"center"}
-              fontFamily={"Chivo"}
-              fontSize={"30px"}
-              fontStyle={"normal"}
-              fontWeight={"400"}
-              lineHeight={"39.9px"}
+            >
+              <Text
+                alignSelf={"stretch"}
+                color={"var(--gray-900, #101828)"}
+                textAlign={"center"}
+                fontFamily={"Chivo"}
+                fontSize={"30px"}
+                fontStyle={"normal"}
+                fontWeight={"400"}
+                lineHeight={"39.9px"}
               >
-              Log in to your account
-            </Text>
+                Log in to your account
+              </Text>
+            </Box>
           </Box>
-              </Box>
 
           {/* #############   CONTNENT   ########################## */}
 
@@ -217,7 +213,9 @@ const page = () => {
                     fontWeight={"400"}
                     lineHeight={"normal"}
                     placeholder="Enter your phone number"
-                    type="text"  value={username} onChange={(e) => setUsername(e.target.value)}
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
                   ></Input>
                 </Box>
               </Box>
@@ -270,13 +268,14 @@ const page = () => {
                     lineHeight={"24px"}
                     placeholder="Enter your password"
                     color={"var(--gray-500, #667085)"}
-                    type="password"  value={password} onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                   ></Input>
                 </Box>
               </Box>
             </Box>
 
-            
             {/* Acrtion */}
             <Box
               display={"flex"}
@@ -296,18 +295,25 @@ const page = () => {
                 alignSelf={"stretch"}
                 color={"#fff"}
                 borderRadius={"6px"}
-                fontFamily={'Inter'}
+                fontFamily={"Inter"}
                 backgroundColor={"var(--primary-main, #11190C)"} // Use the 'bg' prop to set the background color
                 onClick={handleSignIn}
               >
                 Sign in
               </chakra.button>
-              {challengeName === 'NEW_PASSWORD_REQUIRED' && (
-        <div>
-          <input type="password" placeholder="New Password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
-          <button onClick={handleChallengeResponse}>Change Password</button>
-        </div>
-      )}
+              {challengeName === "NEW_PASSWORD_REQUIRED" && (
+                <div>
+                  <input
+                    type="password"
+                    placeholder="New Password"
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                  />
+                  <button onClick={handleChallengeResponse}>
+                    Change Password
+                  </button>
+                </div>
+              )}
             </Box>
           </Box>
         </Box>
