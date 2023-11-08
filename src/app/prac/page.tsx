6b8b8721109d8393705c139042f1d3107898c3d1
Ansar857 <mohammadansar857@gmail.com>
2023-11-08 20:36:50 +0500

    new commit

--- src/app/prac/page.tsx
+++ src/app/prac/page.tsx
@@ -4,53 +4,52 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [file, setFile] = useState<File | null>(null);
+    const [file, setFile] = useState<File | null>(null);
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedFile  = event.target.files && event.target.files[0] ;
-    setFile(selectedFile);
-  };
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = event.target.files && event.target.files[0];
+        setFile(selectedFile);
+    };
 
-  const handleFileUpload = async () => {
-    if (!file) {
-      alert("Please select a file to upload");
-      return;
-    }
-
-    try {
-      const response = await fetch(
-        `https://zp2dhmgwaa.execute-api.us-east-1.amazonaws.com/generatepresignedurl?fileName=${file.name}&contentType=text/plain`
-      );
-      const data = await response.json();
-      console.log(data)
-
-      if (data.uploadUrl) {
-        const uploadResponse = await fetch(data.uploadUrl, {
-          method: "PUT",
-          body: file,
-        });
-
-        if (uploadResponse.ok) {
-          alert("File uploaded successfully!");
-        } else {
-          alert("File upload failed");
+    const handleFileUpload = async () => {
+        if (!file) {
+            alert("Please select a file to upload");
+            return;
         }
-      } else {
-        alert("Failed to get pre-signed URL");
-      }
-    } catch (error) {
-      console.error("Error uploading file:", error);
-      alert("An error occurred while uploading the file.");
-    }
-  };
 
-  return (
-    <div>
-      <h1>Upload a file to S3</h1>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleFileUpload}>Upload</button>
-    </div>
-  );
+        try {
+            const response = await fetch(
+                `https://zp2dhmgwaa.execute-api.us-east-1.amazonaws.com/generatepresignedurl?fileName=${file.name}&contentType=text/plain`
+            );
+            const data = await response.json();
+            console.log(data)
+            if (data.uploadUrl) {
+                const uploadResponse = await fetch(data.uploadUrl, {
+                    method: "PUT",
+                    body: file,
+                });
+
+                if (uploadResponse.ok) {
+                    alert("File uploaded successfully!");
+                } else {
+                    alert("File upload failed");
+                }
+            } else {
+                alert("Failed to get pre-signed URL");
+            }
+        } catch (error) {
+            console.error("Error uploading file:", error);
+            alert("An error occurred while uploading the file.");
+        }
+    };
+
+    return (
+        <div>
+            <h1>Upload a file to S3</h1>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleFileUpload}>Upload</button>
+        </div>
+    );
 };
 
 export default Home;
