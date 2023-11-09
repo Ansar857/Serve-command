3eed1575827ab381202f7589ef6c9490dd118aa9
Ansar857 <mohammadansar857@gmail.com>
2023-11-10 01:42:52 +0500

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
 import { Box, Heading } from "@chakra-ui/react";
 import Title from "./components/Title";
 import Card from "./components/Card";
-import ImportDialogueBox from "./components/ImportDialogueBox";
 import Sidebar from "./components/Sidebar";
 
 export default function Home() {
   return (
     <Box display={"flex"}>
-
       {/* Side bar */}
       <Box>
         <Sidebar />
       </Box>
-      <Box>
 
-        {/* Components */}
+      {/* Components */}
+      <Box>
         <Title />
         <Card />
       </Box>
