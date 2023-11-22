a2e0282ca8bf5030b8a4223553c9a25a9e6a89da
Ansar857 <mohammadansar857@gmail.com>
2023-11-23 01:42:49 +0500

    push

--- src/app/Test/page.tsx
+++ src/app/Test/page.tsx
@@ -1,202 +1,132 @@
-'use client'
-import { Box ,  Image , Text , Input , chakra } from "@chakra-ui/react"
-import React, {useState} from "react";
+"use client";
+import {
+  Box,
+  Button,
+  Input,
+  Flex,
+  Image,
+  Heading,
+  chakra,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  CognitoIdentityProviderClient,
+  RespondToAuthChallengeCommand,
+} from "@aws-sdk/client-cognito-identity-provider";
 
-const page = () => {
-
-
+const Page = () => {
   const [show, setShow] = useState(false);
 
   return (
-    <Box
-    display={'flex'}
-    width={'1920px'}  
-    padding={'349px 0px 337px 0px'}
-    justifyContent={'center'}
-    alignItems={'center'}
-    background={'var(--gray-50, #F7FAFC)'}>
-
-
-      {/* ##################        CONTENT BOX                 ################################# */}
-      <Box
-      display={'flex'}
-      width={'360px'}
-      flexDirection={'column'}
-      alignItems={'center'}
-      gap={'32px'}
-      flexShrink={'0'}>
-
-        {/* Header Box */}
-        <Box
-        display={'flex'}
-        flexDirection={'column'}
-        alignItems={'center'}
-        gap={'24px'}
-        alignSelf={'stretch'}>
-
-          {/* Logo */}
-          <Image src="/Flattened.svg" height={'33px'} width={'34px'}/>
-
-          {/* Text and supporting text */}
-          <Box
-          display={'flex'}
-          flexDirection={'column'}
-          alignItems={'flex-start'}
-          gap={'12px'}
-          alignSelf={'stretch'}>
-
-            {/* Text */}
-            <Text
-            alignSelf={'stretch'}
-            color={'var(--gray-900, #101828)'}
-            textAlign={'center'}
-            fontFamily={'Chivo'}
-            fontSize={'30px'}
-            fontStyle={'normal'}
-            fontWeight={'400'}
-            lineHeight={'39.9px'}>
-              Reset Password</Text>
-
-            {/* Text */}
-            <Text
-            alignSelf={'stretch'}
-            color={'var(--gray-600, #475467)'}
-            textAlign={'center'}
-            fontFamily={'Inter'}
-            fontSize={'16px'}
-            fontStyle={'normal'}
-            fontWeight={'400'}
-            lineHeight={'24px'}>
-              No worries, we will help you reset your password.</Text>
+    <Flex flexDir={"column"} height={"100vh"} bg={"var(--gray-50, #F7FAFC)"}>
+      <Flex
+        flexDir={"column"}
+        width={{ sm: "", lg: "20%", "2xl": "360px" }}
+        margin={"auto"}
+        gap={{ sm: "20px", lg: "26px", "2xl": "32px" }}
+      >
+        <Flex
+          flexDir={"column"}
+          gap={{ sm: "10px", lg: "16px", "2xl": "20px" }}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Image
+            src={"/Flattened.svg"}
+            width={{ sm: "28px", "2xl": "33px" }}
+            height={{ sm: "28px", "2xl": "33px" }}
+            alt={""}
+            fill={"var(--primary-main, #11190C)"}
+          />
+          <Box alignSelf={"stretch"}>
+            <Heading
+              color={"var(--gray-900, #101828)"}
+              textAlign={"center"}
+              fontFamily={"Chivo"}
+              fontSize={{ sm: "18px", lg: "22px", "2xl": "30px" }}
+              fontStyle={"normal"}
+              fontWeight={400}
+              lineHeight={"39.9px"}
+            >
+              Set New Password
+            </Heading>
           </Box>
-
-        </Box>
-
-        {/* Lower Content Box */}
+        </Flex>
         <Box
-        display={'flex'}
-        flexDirection={'column'}
-        alignItems={'center'}
-        gap={'24px'}
-        alignSelf={'stretch'}
-        borderRadius={'12px'}>
-
-          {/* Form */}
+          display={"flex"}
+          flexDir={"column"}
+          gap={{ sm: "18px", "2xl": "24px" }}
+        >
           <Box
-          display={'flex'}
-          flexDirection={'column'}
-          alignItems={'flex-start'}
-          gap={'20px'}
-          alignSelf={'stretch'}>
-
-            {/*  */}
-            <Box
-            display={'flex'}
-            flexDirection={'column'}
-            alignItems={'flex-start'}
-            gap={'4px'}
-            alignSelf={'stretch'}>
-
-              {/* Input Text */}
-              <Text
-              height={'16px'}
-              alignSelf={'stretch'}
-              color={'var(--black-alpha-700, rgba(0, 0, 0, 0.64))'}
-              fontFamily={'Inter'}
-              fontSize={'12px'}
-              fontStyle={'normal'}
-              fontWeight={'600'}
-              lineHeight={'16px'}
-              letterSpacing={'0.3px'}>
-                Phone Number</Text>
-
-                {/* Input Group */}
-                <Box
-                  display={"flex"}
-                  alignItems={"flex-start"}
-                  alignSelf={"stretch"}
+            display={"flex"}
+            flexDir={"column"}
+            gap={{ sm: "20px", "2xl": "20px" }}
+          >
+            {/* phone */}
+            <Flex flexDir={"column"} gap={{ sm: "4px", "2xl": "4px" }}>
+              <Box height={{ sm: "", "2xl": "16px" }} alignSelf={"stretch"}>
+                <Text
+                  color={"var(--black-alpha-700, rgba(0, 0, 0, 0.64))"}
+                  fontFamily={"Inter"}
+                  fontSize={{ sm: "12px", lg: "14px", "2xl": "12px" }}
+                  fontStyle={"normal"}
+                  fontWeight={600}
+                  lineHeight={"16px"}
+                  letterSpacing={"0.3px"}
                 >
-                  <Input
-                    display={"flex"}
-                    height={"40px"}
-                    padding={"0px 16px"}
-                    flexDirection={"column"}
-                    justifyContent={"center"}
-                    alignItems={"center"}
-                    gap={"10px"}
-                    flex={"1 0 0"}
-                    borderRadius={"4px"}
-                    border={"1px solid var(--gray-200, #E2E8F0)"}
-                    background={"var(--white, #FFF)"}
-                    alignSelf={"stretch"}
-                    fontFamily={"Inter"}
-                    fontSize={"16px"}
-                    fontStyle={"normal"}
-                    fontWeight={"400"}
-                    lineHeight={"normal"}
-                    placeholder="Enter your phone number"
-                    type="text"
-                    
-                  ></Input>
-                </Box>
-                
-
-            </Box>
-
+                  New Password
+                </Text>
+              </Box>
+              <Box
+                display={"flex"}
+                alignItems={"flex-start"}
+                alignSelf={"stretch"}
+              >
+                <Input
+                  type="text"
+                  name="phone"
+                  placeholder="Enter New Password"
+                  color={"var(--gray-400, #A0AEC0)"}
+                  fontFamily={"Inter"}
+                  fontSize={{ sm: "14px", "2xl": "16px" }}
+                  fontStyle={"normal"}
+                  fontWeight={400}
+                  lineHeight={"normal"}
+                  background={"var(--white, #FFF)"}
+                />
+              </Box>
+            </Flex>
+            {/* password */}
+           
           </Box>
-
-          {/* ##########      Actions           ######### * }
-
-          {/* Send Code Button */}
-          <chakra.button
-          display={'flex'}
-          height={'40px'}
-          padding={'0px 16px'}
-          justifyContent={'center'}
-          alignItems={'center'}
-          gap={'8px'}
-          alignSelf={'stretch'}
-          borderRadius={'6px'}
-          background={'var(--primary-main, #11190C)'}>
-            <Text
-            color={'#FFF'}
-            fontFamily={'Inter'}
-            fontSize={'16px'}
-            fontStyle={'normal'}
-            fontWeight={'600'}
-            lineHeight={'24px'}>
-              Send Code</Text>
-          </chakra.button>
-
-          {/* Back to login Button */}
-          <chakra.button
-          display={'flex'}
-          height={'40px'}
-          padding={'0px 16px'}
-          justifyContent={'center'}
-          alignItems={'center'}
-          gap={'8px'}
-          alignSelf={'stretch'}
-          borderRadius={'6px'}>
-            <Image src="/left-icon.svg" width={'16px'} height={'16px'} />
-            <Text
-            color={'var(--gray-800, #1A202C)'}
-            fontFamily={'Inter'}
-            fontSize={'16px'}
-            fontStyle={'normal'}
-            fontWeight={'600'}
-            lineHeight={'24px'}>
-              Back to log in</Text>
-          </chakra.button>
-
-
         </Box>
-        
+        <Box
+          display={"flex"}
+          height={{ sm: "", lg: "34px", "2xl": "40px" }}
+          padding={{ sm: "", "2xl": "0px 16px" }}
+          justifyContent={"center"}
+          alignItems={"center"}
+          gap={{ sm: "", "2xl": "8px" }}
+          alignSelf={"stretch"}
+          borderRadius={{ sm: "4px", md: "6px", "2xl": "6px" }}
+          background={"var(--primary-main, #11190C)"}
+        >
+          <chakra.button
+            color={"#FFF"}
+            fontFamily={"Inter"}
+            fontSize={{ sm: "", "2xl": "16px" }}
+            fontStyle={"normal"}
+            fontWeight={"600"}
+            lineHeight={"24px"}
+            _hover={{ background: "none" }}
+          >
+            Change Password
+          </chakra.button>
+        </Box>
+      </Flex>
+    </Flex>
+  );
+};
 
-      </Box>
-
-    </Box>
-  )
-}
-
-export default page
+export default Page;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Image, Input, Text, chakra } from "@chakra-ui/react";
+import { Box, Image, Input, Text, chakra , Flex , Heading } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import {CognitoIdentityProviderClient,InitiateAuthCommand,RespondToAuthChallengeCommand,} from "@aws-sdk/client-cognito-identity-provider";
@@ -20,7 +20,6 @@ const page = () => {
   const [newPassword, setNewPassword] = useState("");
   const [session, setSession] = useState("");
   const [challengeName, setChallengeName] = useState<string | null>(null);
-  // const [challengeParams, setChallengeParams] = useState<any>({});
   const [message, setMessage] = useState("");
   const router = useRouter();
 
@@ -340,17 +339,128 @@ const page = () => {
                   Sign in</Text>
               </chakra.button>
               {challengeName === "NEW_PASSWORD_REQUIRED" && (
-                <div>
-                  <input
-                    type="password"
-                    placeholder="New Password"
-                    value={newPassword}
-                    onChange={(e) => setNewPassword(e.target.value)}
-                  />
-                  <button onClick={handleChallengeResponse}>
-                    Change Password
-                  </button>
-                </div>
+                // <div>
+                //   <input
+                //     type="password"
+                //     placeholder="New Password"
+                //     value={newPassword}
+                //     onChange={(e) => setNewPassword(e.target.value)}
+                //   />
+                //   <button onClick={handleChallengeResponse}>
+                //     Change Password
+                //   </button>
+                // </div>
+                <Flex flexDir={"column"} height={"100vh"} bg={"var(--gray-50, #F7FAFC)"}>
+                <Flex
+                  flexDir={"column"}
+                  width={{ sm: "", lg: "20%", "2xl": "360px" }}
+                  margin={"auto"}
+                  gap={{ sm: "20px", lg: "26px", "2xl": "32px" }}
+                >
+                  <Flex
+                    flexDir={"column"}
+                    gap={{ sm: "10px", lg: "16px", "2xl": "20px" }}
+                    justifyContent={"center"}
+                    alignItems={"center"}
+                  >
+                    <Image
+                      src={"/Flattened.svg"}
+                      width={{ sm: "28px", "2xl": "33px" }}
+                      height={{ sm: "28px", "2xl": "33px" }}
+                      alt={""}
+                      fill={"var(--primary-main, #11190C)"}
+                    />
+                    <Box alignSelf={"stretch"}>
+                      <Heading
+                        color={"var(--gray-900, #101828)"}
+                        textAlign={"center"}
+                        fontFamily={"Chivo"}
+                        fontSize={{ sm: "18px", lg: "22px", "2xl": "30px" }}
+                        fontStyle={"normal"}
+                        fontWeight={400}
+                        lineHeight={"39.9px"}
+                      >
+                        Set New Password
+                      </Heading>
+                    </Box>
+                  </Flex>
+                  <Box
+                    display={"flex"}
+                    flexDir={"column"}
+                    gap={{ sm: "18px", "2xl": "24px" }}
+                  >
+                    <Box
+                      display={"flex"}
+                      flexDir={"column"}
+                      gap={{ sm: "20px", "2xl": "20px" }}
+                    >
+                      {/* phone */}
+                      <Flex flexDir={"column"} gap={{ sm: "4px", "2xl": "4px" }}>
+                        <Box height={{ sm: "", "2xl": "16px" }} alignSelf={"stretch"}>
+                          <Text
+                            color={"var(--black-alpha-700, rgba(0, 0, 0, 0.64))"}
+                            fontFamily={"Inter"}
+                            fontSize={{ sm: "12px", lg: "14px", "2xl": "12px" }}
+                            fontStyle={"normal"}
+                            fontWeight={600}
+                            lineHeight={"16px"}
+                            letterSpacing={"0.3px"}
+                          >
+                            New Password
+                          </Text>
+                        </Box>
+                        <Box
+                          display={"flex"}
+                          alignItems={"flex-start"}
+                          alignSelf={"stretch"}
+                        >
+                          <Input
+                
+                            name="phone"
+                            placeholder="Enter New Password"
+                            color={"var(--gray-400, #A0AEC0)"}
+                            fontFamily={"Inter"}
+                            fontSize={{ sm: "14px", "2xl": "16px" }}
+                            fontStyle={"normal"}
+                            fontWeight={400}
+                            lineHeight={"normal"}
+                            background={"var(--white, #FFF)"}
+                            type="password"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                          />
+                        </Box>
+                      </Flex>
+                      {/* password */}
+                     
+                    </Box>
+                  </Box>
+                  <Box
+                    display={"flex"}
+                    height={{ sm: "", lg: "34px", "2xl": "40px" }}
+                    padding={{ sm: "", "2xl": "0px 16px" }}
+                    justifyContent={"center"}
+                    alignItems={"center"}
+                    gap={{ sm: "", "2xl": "8px" }}
+                    alignSelf={"stretch"}
+                    borderRadius={{ sm: "4px", md: "6px", "2xl": "6px" }}
+                    background={"var(--primary-main, #11190C)"}
+                  >
+                    <chakra.button
+                      color={"#FFF"}
+                      fontFamily={"Inter"}
+                      fontSize={{ sm: "", "2xl": "16px" }}
+                      fontStyle={"normal"}
+                      fontWeight={"600"}
+                      lineHeight={"24px"}
+                      _hover={{ background: "none" }}
+                      onClick={handleChallengeResponse}
+                    >
+                      Change Password
+                    </chakra.button>
+                  </Box>
+                </Flex>
+              </Flex>
               )}
             </Box>
           </Box>
