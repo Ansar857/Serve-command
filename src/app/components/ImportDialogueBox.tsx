8538b64d7d41dc63a7243c22586eb9f50f672f1e
Ansar857 <mohammadansar857@gmail.com>
2023-11-24 00:34:43 +0500

    push

--- src/app/components/ImportDialogueBox.tsx
+++ src/app/components/ImportDialogueBox.tsx
@@ -17,6 +17,7 @@ const ImportDialogueBox = ({onClose}:any) => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const isSubmitDisabled = !selectedFile;
   const [uploadProgress, setUploadProgress] = useState<number>(0);
+  const [progress , setProgress] = useState(false)
 
   // const [file, setFile] = useState<File | null>(null);
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -59,6 +60,7 @@ const ImportDialogueBox = ({onClose}:any) => {
           console.log("File uploaded successfully!");
           onClose();
           handleClear();
+          setProgress(true)
         } else {
           alert("File upload failed ");
           console.log(uploadResponse.data)
@@ -81,6 +83,69 @@ const ImportDialogueBox = ({onClose}:any) => {
 
   return (
     <>
+  
+    {progress? (
+      <Box
+      display={"flex"}
+      justifyContent={"center"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      // h={{"2xl":"1080px"}}
+      // w={{"2xl":"1920px"}}
+    >
+      <Box
+        display={"flex"}
+        w={{ "2xl": "432px" }}
+        flexDirection={"column"}
+        alignItems={"flex-start"}
+        gap={{ "2xl": "32px" }}
+        borderRadius={"24px"}
+        background={"var(--white-100, #FFF)"}
+        boxShadow={
+          "0px 24px 48px 0px rgba(90, 91, 106, 0.08), 0px 12px 24px 0px rgba(58, 58, 68, 0.08)"
+        }
+        p={{ "2xl": "32px" }}
+      >
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          alignSelf={"stretch"}
+        >
+          <Text
+            textColor={"var(--gray-100, #19191D)"}
+            fontFamily={"Inter"}
+            fontSize={{ "2xl": "20px" }}
+            fontStyle={"normal"}
+            fontWeight={600}
+            lineHeight={{ "2xl": "28px" }}
+          >
+            Import in Progress...{uploadProgress}
+          </Text>
+          <Progress value={90} />
+        </Box>
+        <Box
+          w={{ "2xl": "368px" }}
+          h={{ "2xl": "8px" }}
+          flexShrink={0}
+          borderRadius={"4px"}
+          background={"var(--primary-states-hover, rgba(17, 25, 12, 0.04))"}
+        >
+          <Progress
+            w={"100%"}
+            h={{ "2xl": "8px" }}
+            flexShrink={0}
+            borderRadius={"4px"}
+            value={uploadProgress}
+            colorScheme={"blackAlpha"}
+          />
+        </Box>
+      </Box>
+        </Box>
+    ):
+    (
+
           <Box
             display={"flex"}
             width={"900px"}
@@ -485,6 +550,7 @@ const ImportDialogueBox = ({onClose}:any) => {
               </Box>
             </Box>
           </Box>
+    )}
     </>
   );
 };
