86033a23abd0d0613bc884c9ca06f17e8e62ed84
Ansar857 <mohammadansar857@gmail.com>
2023-11-24 02:58:43 +0500

    NEW COMMIT

--- src/app/components/CompleteSuccess.tsx
+++ src/app/components/CompleteSuccess.tsx
@@ -1,19 +1,16 @@
-// CompleteSuccess.tsx
 import React from "react";
 import { Box, Button, Text } from "@chakra-ui/react";
 
-const CompleteSuccess = ({ onClose }: any) => {
-  return (
-    <Box
-      display={"flex"}
-      flexDirection={"column"}
-      alignItems={"center"}
-      justifyContent={"center"}
-    >
-      <Text>Your upload was successful!</Text>
-      <Button onClick={onClose}>Done</Button>
-    </Box>
-  );
-};
-
-export { CompleteSuccess };
+export const CompleteSuccess = ({ onClose }: any) => {
+  return ( 
+  <Box
+    display={"flex"}
+    flexDirection={"column"}
+    alignItems={"center"}
+    justifyContent={"center"}
+  >
+    <Text>Your upload was successful!</Text>
+    <Button onClick={onClose}>Done</Button>
+  </Box>
+  )
+}
