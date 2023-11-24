8a18fd456b2d309825d5e2f834c6b3dfbee6e5d4
Ansar857 <mohammadansar857@gmail.com>
2023-11-24 21:38:15 +0500

    push

--- src/app/components/ImportSuccessful.tsx
+++ src/app/components/ImportSuccessful.tsx
@@ -6,63 +6,70 @@ const page = () => {
     const [isDone, setIsDone] = useState<boolean>(false);
 
     return (
+        <>
+        {isDone? (
                 <Box
-                    display={"flex"}
-                    width={{ sm: "", "lg": "432px" }}
-                    padding={{ sm: "", "lg": "32px" }}
-                    flexDir={"column"}
-                    justifyContent={"center"}
-                    alignItems={"center"}
-                    gap={{ sm: "", "lg": "32px" }}
-                    borderRadius={'24px'}
-                    bg={'var(--white-100, #FFF)'}
-                    shadow={'0px 24px 48px 0px rgba(90, 91, 106, 0.08), 0px 12px 24px 0px rgba(58, 58, 68, 0.08)'}
+                display={"flex"}
+                width={{ sm: "", "lg": "432px" }}
+                padding={{ sm: "", "lg": "32px" }}
+                flexDir={"column"}
+                justifyContent={"center"}
+                alignItems={"center"}
+                gap={{ sm: "", "lg": "32px" }}
+                borderRadius={'24px'}
+                bg={'var(--white-100, #FFF)'}
+                shadow={'0px 24px 48px 0px rgba(90, 91, 106, 0.08), 0px 12px 24px 0px rgba(58, 58, 68, 0.08)'}
+            >
+                <Box>
+                    <Text
+                        color={"var(--primary-main, #11190C)"}
+                        fontFamily={"Inter"}
+                        fontSize={{ sm: "16px", lg: "20px" }}
+                        fontStyle={"normal"}
+                        fontWeight={{ sm: "400", lg: "600" }}
+                        lineHeight={{ sm: "20px", lg: "28px" }}
+                    >
+                        Import Successful!
+                    </Text>
+                </Box>
+                <Box
+                    width={{ sm: "", "lg": "117px" }}
+                    height={{ sm: "", "lg": "117px" }}
                 >
-                    <Box>
+                    <Image src={"/checkCircle.svg"} alt={"circle"} />
+                </Box>
+                <chakra.button
+                    onClick={() => setIsDone(true)}
+                >
+                    <Box
+                        display={"flex"}
+                        alignItems={"center"}
+                        justifyContent={"center"}
+                        gap={{ sm: "", "lg": "8px" }}
+                        height={{ sm: "", "lg": "32px" }}
+                        padding={{ sm: "", "lg": "0px 12px" }}
+                        border={"1px solid var(--gray-200, #E2E8F0)"}
+                        borderRadius={"6px"}
+                        background={"#000"}
+                    >
                         <Text
-                            color={"var(--primary-main, #11190C)"}
+                            color={" #FFF"}
                             fontFamily={"Inter"}
-                            fontSize={{ sm: "16px", lg: "20px" }}
+                            fontSize={{ sm: "", "lg": "14px" }}
                             fontStyle={"normal"}
-                            fontWeight={{ sm: "400", lg: "600" }}
-                            lineHeight={{ sm: "20px", lg: "28px" }}
+                            fontWeight={{ sm: "", "lg": "600" }}
+                            lineHeight={{ sm: "20px", "lg": "20px" }}
                         >
-                            Import Successful!
+                            Done
                         </Text>
                     </Box>
-                    <Box
-                        width={{ sm: "", "lg": "117px" }}
-                        height={{ sm: "", "lg": "117px" }}
-                    >
-                        <Image src={"/checkCircle.svg"} alt={"circle"} />
-                    </Box>
-                    <chakra.button
-                        onClick={() => setIsDone(true)}
-                    >
-                        <Box
-                            display={"flex"}
-                            alignItems={"center"}
-                            justifyContent={"center"}
-                            gap={{ sm: "", "lg": "8px" }}
-                            height={{ sm: "", "lg": "32px" }}
-                            padding={{ sm: "", "lg": "0px 12px" }}
-                            border={"1px solid var(--gray-200, #E2E8F0)"}
-                            borderRadius={"6px"}
-                            background={"#000"}
-                        >
-                            <Text
-                                color={" #FFF"}
-                                fontFamily={"Inter"}
-                                fontSize={{ sm: "", "lg": "14px" }}
-                                fontStyle={"normal"}
-                                fontWeight={{ sm: "", "lg": "600" }}
-                                lineHeight={{ sm: "20px", "lg": "20px" }}
-                            >
-                                Done
-                            </Text>
-                        </Box>
-                    </chakra.button>
-                </Box>
+                </chakra.button>
+        </Box>
+        ) : (
+            <div>Hello</div>
+        ) }
+           
+            </>
     )
 }
 
