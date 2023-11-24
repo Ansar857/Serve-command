f04552ecfbfccb59caa4f0875b7f880ff512170a
Ansar857 <mohammadansar857@gmail.com>
2023-11-24 20:49:49 +0500

    push

--- src/app/components/ImportDialogueBox.tsx
+++ src/app/components/ImportDialogueBox.tsx
@@ -10,6 +10,7 @@ import {
 import React, { useRef, useState } from "react";
 import axios from "axios";
 import ImportSuccessful from "./ImportSuccessful";
+import { ProgressBar } from "./ProgressBar";
 
 const ImportDialogueBox = ({onClose}:any) => {
   const cancelRef: any = React.useRef();
@@ -82,64 +83,7 @@ const ImportDialogueBox = ({onClose}:any) => {
 {uploadProgress ? 
             (uploadProgress === 100 ? (<ImportSuccessful/>) : (
               // PROGRESS BAR
-            <Box
-            display={"flex"}
-            justifyContent={"center"}
-            flexDirection={"column"}
-            alignItems={"center"}
-            // h={{"2xl":"1080px"}}
-            // w={{"2xl":"1920px"}}
-          >
-            <Box
-              display={"flex"}
-              w={{ "2xl": "432px" }}
-              flexDirection={"column"}
-              alignItems={"flex-start"}
-              gap={{ "2xl": "32px" }}
-              borderRadius={"24px"}
-              background={"var(--white-100, #FFF)"}
-              boxShadow={
-                "0px 24px 48px 0px rgba(90, 91, 106, 0.08), 0px 12px 24px 0px rgba(58, 58, 68, 0.08)"
-              }
-              p={{ "2xl": "32px" }}
-            >
-              <Box
-                display={"flex"}
-                flexDirection={"column"}
-                justifyContent={"center"}
-                alignItems={"center"}
-                alignSelf={"stretch"}
-              >
-                <Text
-                  textColor={"var(--gray-100, #19191D)"}
-                  fontFamily={"Inter"}
-                  fontSize={{ "2xl": "20px" }}
-                  fontStyle={"normal"}
-                  fontWeight={600}
-                  lineHeight={{ "2xl": "28px" }}
-                >
-                  Import in Progress...{uploadProgress}
-                </Text>
-                <Progress value={90} />
-              </Box>
-              <Box
-                w={{ "2xl": "368px" }}
-                h={{ "2xl": "8px" }}
-                flexShrink={0}
-                borderRadius={"4px"}
-                background={"var(--primary-states-hover, rgba(17, 25, 12, 0.04))"}
-              >
-                <Progress
-                  w={"100%"}
-                  h={{ "2xl": "8px" }}
-                  flexShrink={0}
-                  borderRadius={"4px"}
-                  value={uploadProgress}
-                  colorScheme={"blackAlpha"}
-                />
-              </Box>
-            </Box>
-              </Box>)) :
+            <ProgressBar uploadProgress={uploadProgress} />)) :
                 (<Box
                   display={"flex"}
                   width={"900px"}
